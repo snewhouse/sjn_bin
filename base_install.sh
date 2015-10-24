7fde5d1a11dc8bf626f8ee0d3d091e650255445d
Stephen Newhouse <stephen.j.newhouse@gmail.com>
2015-10-24 18:04:52 +0000

    added base_install.sh from our ngseasy dockerfiles. basic linux tools

---
+++ base_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+sudo apt-get install \
+apt-utils \
+  automake \
+  ant \
+  bash \
+  binutils \
+  perl \
+  bioperl \
+  build-essential \
+  bzip2 \
+  c++11 \
+  cdbs \
+  cmake \
+  cron \
+  curl \
+  dkms \
+  dpkg-dev \
+  g++ \
+  gpp \
+  gcc \
+  gfortran \
+  git \
+  git-core \
+  libblas-dev \
+  libatlas-dev \
+  libbz2-dev \
+  liblzma-dev \
+  libpcre3-dev \
+  libreadline-dev \
+  make \
+  mercurial \
+  php5-curl \
+  python python-dev python-yaml ncurses-dev zlib1g-dev python-numpy python-pip \
+  sudo \
+  subversion \
+  tabix \
+  tree \
+  unzip \
+  vim \
+  wget \
+  python-software-properties \
+  libc-bin \
+  llvm \
+  libconfig-dev \
+  ncurses-dev \
+  zlib1g-dev \
+  yum \
+  libX11-dev libXpm-dev libXft-dev libXext-dev \
+  asciidoc
